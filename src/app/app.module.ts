a808d80b2d4bdca1f94fea6ca4db5f0b6c7a7635
unhignedporter <unhingedporter@gmail.com>
2021-08-08 14:21:37 +0530

    Added dummy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterSectionComponent } from './filter-section/filter-section.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
+import { TopicCardComponent } from './topic-card/topic-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterSectionComponent,
+    HeaderSectionComponent,
+    FooterSectionComponent,
+    TopicCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/filter-section/filter-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-section',
+  templateUrl: './filter-section.component.html',
+  styleUrls: ['./filter-section.component.scss']
+})
+export class FilterSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
